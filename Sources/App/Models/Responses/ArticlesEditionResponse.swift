fb8e941828163af3ffcd4b11ebb04691bc114dc3
Pedro Carrasco <pedrommcarrasco@gmail.com>
2019-08-09 15:40:40 +0100

    Added description to ArticlesEditionResponse

--- Sources/App/Models/Responses/ArticlesEditionResponse.swift
+++ Sources/App/Models/Responses/ArticlesEditionResponse.swift
@@ -14,12 +14,14 @@ final class ArticlesEditionResponse {
     
     // MARK: Properties
     let title: String
+    let description: String
     let articles: [Article]
     let curators: [Person]
     
     // MARK: Init
     init(edition: ArticlesEdition, articles: [Article]) {
         self.title = edition.title
+        self.description = edition.description
         self.articles = articles
         self.curators = Set(articles.map { return $0.curator }).sorted()
     }
